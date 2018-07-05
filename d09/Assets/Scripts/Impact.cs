7eab02ba73f3f68d3b9127c02943eed385f855b2
Arthur Poisson <apoisson@student.42.fr>
2018-07-05 17:22:10 +0200

    Correctifs d09

--- d09/Assets/Scripts/Impact.cs
+++ d09/Assets/Scripts/Impact.cs
@@ -3,14 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Impact : MonoBehaviour {
+    void Start () {
+        Destroy(gameObject, GetComponent<ParticleSystem>().main.duration);
+    }
 
-	void Start () {
-		Destroy(gameObject, GetComponent<ParticleSystem>().main.duration);
-	}
-
-	void OnTriggerEnter (Collider other) {
-		if (gameObject.tag == "ZodSpark")
-			if (other.gameObject.tag == "Zombie" && other.GetType() == typeof(CapsuleCollider))
-				other.GetComponent<Enemy>().TakeDamage(20);
-	}
+    void OnTriggerEnter (Collider other) {
+        if (gameObject.tag == "ZodSpark")
+            if (other.gameObject.tag == "Zombie" && other.GetType() == typeof(CapsuleCollider))
+                other.GetComponent<Enemy>().TakeDamage(20);
+    }
 }
